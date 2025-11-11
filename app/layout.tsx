0d03bacf1592965fafcdc09b1e20761aa8aec7e3
bakxriyev <kamronbekbahriyev18@gmail.com>
2025-11-11 15:55:44 +0500

    dssd

--- app/layout.tsx
+++ app/layout.tsx
@@ -19,12 +19,36 @@ export default function RootLayout({
   children,
 }: Readonly<{
   children: React.ReactNode
-
-
-  
 }>) {
   return (
     <html lang="uz">
+      <head>
+        {/* Meta Pixel Code */}
+        {/* eslint-disable-next-line @next/next/no-sync-scripts */}
+        <script dangerouslySetInnerHTML={{
+          __html: `!function(f,b,e,v,n,t,s)
+{if(f.fbq)return;n=f.fbq=function(){n.callMethod?
+n.callMethod.apply(n,arguments):n.queue.push(arguments)};
+if(!f._fbq)f._fbq=n;n.push=n;n.loaded=!0;n.version='2.0';
+n.queue=[];t=b.createElement(e);t.async=!0;
+t.src=v;s=b.getElementsByTagName(e)[0];
+s.parentNode.insertBefore(t,s)}(window, document,'script',
+'https://connect.facebook.net/en_US/fbevents.js');
+fbq('init', '1916942475831227');
+fbq('track', 'PageView');`
+        }} />
+        <noscript>
+          <img
+            height="1"
+            width="1"
+            style={{ display: "none" }}
+            src="https://www.facebook.com/tr?id=1916942475831227&ev=PageView&noscript=1"
+            alt=""
+          />
+        </noscript>
+        {/* End Meta Pixel Code */}
+      </head>
+
       <body className={`font-sans antialiased ${inter.className}`}>
         {children}
       </body>
